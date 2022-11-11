03e02f8c5671fbff6076c080d3a966279dfd68b8
Sylwia Brant <sylwia.brant@tesco.com>
2022-11-11 11:09:36 +0100

    Add react router

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux'
 import store from './store'
 import './index.css';
 import './assets/fonts/Martel_Sans/MartelSans-Regular.ttf';
-import App from './App';
-import Menu from './pages/Menu/Menu';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Search from './pages/Search/Search';
+import Menu from './pages/Menu/Menu';
+import Error from './pages/Error/Error';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Search/>,
+        errorElement: <Error/>
+    },
+    {
+        path: "Menu",
+        element: <Menu/>,
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <Menu />
+        <RouterProvider router={router}/>
     </Provider>
 );
 
--- ui/src/pages/Search/Search.js
+++ ui/src/pages/Search/Search.js
@@ -5,6 +5,11 @@ import SearchBar from "../../components/SearchBar/SearchBar";
 function Search() {
   return (
     <div className="Search">
+        <div className={ "banner" }>
+            <h1>Twoje ulubione jedzenie w okolicy</h1>
+            <p>Zamów teraz i otrzymaj zniżkę 20zł na następne zamówienie.
+                Skorzystaj z wyszukiwarki, aby znaleźć swoją ulubioną restauracje!</p>
+        </div>
         <div className={"searchTop"}>
             <SearchBar/>
         </div>
