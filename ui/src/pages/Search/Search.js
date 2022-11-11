f7bebc48ae54714052fab3811a3e4496325d8df0
Sylwia Brant <sylwia.brant@tesco.com>
2022-11-11 10:41:57 +0100

    Add search page

--- ui/src/pages/Search/Search.js
+++ ui/src/pages/Search/Search.js
@@ -1,25 +1,15 @@
 import './Search.css';
-import MenuCard from './MenuCard';
 import SearchBar from "../../components/SearchBar/SearchBar";
 
-let items=['Item 1','Item 2','Item 3','Item 4','Item 5'];
-let itemList=[];
-items.forEach((item,index)=>{
-    // itemList.push( <li key={index}>{item}</li>)
-    itemList.push( <MenuCard key={index}>{item}</MenuCard>)
-})
 
-function Menu() {
+function Search() {
   return (
-    <div className="Menu">
+    <div className="Search">
         <div className={"searchTop"}>
             <SearchBar/>
-        </div>
-        <div className={"mainContainer"}>
-            {itemList}
         </div>
     </div>
   );
 }
 
-export default Menu;
+export default Search;
