8b1fa4de46759dfc3c4c334b6529af262c2b9a66
Sylwia Brant <sylwia.brant@tesco.com>
2022-11-11 14:52:53 +0100

    Add Search page

--- ui/src/pages/Search/Search.js
+++ ui/src/pages/Search/Search.js
@@ -3,18 +3,21 @@ import SearchBar from "../../components/SearchBar/SearchBar";
 
 
 function Search() {
-  return (
-    <div className="Search">
-        <div className={ "banner" }>
-            <h1>Twoje ulubione jedzenie w okolicy</h1>
-            <p>Zamów teraz i otrzymaj zniżkę 20zł na następne zamówienie.
-                Skorzystaj z wyszukiwarki, aby znaleźć swoją ulubioną restauracje!</p>
+    return (
+        <div className="Search">
+            <div className={"banner"}>
+                <div className={"writing"}>
+                    <h1>Twoje ulubione jedzenie w okolicy</h1>
+                    <p>Zamów teraz i otrzymaj zniżkę 20zł na następne zamówienie.</p>
+                    <p>Skorzystaj z wyszukiwarki, aby znaleźć swoją ulubioną restauracje!</p>
+                </div>
+                <div className={"searchTop"}>
+                    <SearchBar/>
+                </div>
+            </div>
+            <div className={"restaurants"}></div>
         </div>
-        <div className={"searchTop"}>
-            <SearchBar/>
-        </div>
-    </div>
-  );
+    );
 }
 
 export default Search;
