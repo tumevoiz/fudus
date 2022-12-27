653b6fcff91b98be2c4ac34fea0d00ae04f99eaf
SylwiaBrant <sylwia.brant@o2.pl>
2022-12-27 14:11:02 +0100

    Add basket 3

--- ui/src/reducers/basket.js
+++ ui/src/reducers/basket.js
@@ -23,19 +23,39 @@ const addItemToBasket = (menuItem, basket) => {
     }
 }
 
+const removeItemFromBasket = (id, basket) => {
+    console.log(basket)
+    if (basket === undefined) {
+        return basket
+    }
+
+    let foundId = R.findIndex(R.propEq('id', id))(basket)
+    console.log('id ' + foundId)
+    if (id === -1) {
+        return basket
+    } else {
+        if (basket[foundId].count === 1){
+            return R.remove(R.lastIndexOf(foundId), 1, basket)
+        } else {
+            basket[foundId].count -= 1
+            console.log(basket)
+            return basket
+        }
+    }
+}
+
 const basketReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_MENU_ITEM_TO_BASKET:
             console.log(state.basket)
             return {
                 ...state,
-                // basket: R.append(action.payload, state.basket),
                 basket: addItemToBasket(action.payload, state.basket),
             }
         case REMOVE_MENU_ITEM_FROM_BASKET:
             return {
                 ...state,
-                basket: R.remove(R.lastIndexOf(action.payload), 1, state.basket),
+                basket: removeItemFromBasket(action.payload, state.basket)
             }
         default:
             return state
