07d37e9fdeafff2d6622ad4fab1ea1601338a9bc
SylwiaBrant <sylwia.brant@o2.pl>
2023-01-02 17:36:12 +0100

    Handle redirects

--- api/src/main/resources/db/migration/V3.1__insert_credentials_data.sql
+++ api/src/main/resources/db/migration/V3.1__insert_credentials_data.sql
@@ -1,5 +1,5 @@
-INSERT INTO credentials VALUES ('b70333a6-50e5-4495-a2b2-85b91f06c6b7', 'kkononowicz', '123', '9d261fbe-5f37-4fab-a5d6-15eaa663f4eb');
-INSERT INTO credentials VALUES ('2e60d3e5-9b8d-4bfd-84b4-380d9f41985e', 'mschodolski', 'mschodolski', 'c9c1d698-e77b-4f6d-8d2f-7c87242e4618');
+INSERT INTO credentials VALUES ('b70333a6-50e5-4495-a2b2-85b91f06c6b7', 'kkononowicz', '123admin*', '9d261fbe-5f37-4fab-a5d6-15eaa663f4eb');
+INSERT INTO credentials VALUES ('2e60d3e5-9b8d-4bfd-84b4-380d9f41985e', 'mschodolski', '123mschodolski*', 'c9c1d698-e77b-4f6d-8d2f-7c87242e4618');
 
 INSERT INTO customer VALUES ('9d261fbe-5f37-4fab-a5d6-15eaa663f4eb', 'kkononowicz@fudus.pl', 'Szkolna 17', 'Bialystok', 'Admin');
 INSERT INTO customer VALUES ('c9c1d698-e77b-4f6d-8d2f-7c87242e4618', 'mschodolski@fudus.pl', 'Szkolna 17', 'Bialystok', 'User');
--- ui/src/api/order.js
+++ ui/src/api/order.js
@@ -3,10 +3,5 @@ import axios from "axios";
 const orderEndpoint = 'http://localhost:8080/ordering'
 
 export const placeOrder = async (order) => {
-    return await axios.post(orderEndpoint, {
-        params: {order},
-        headers: {
-            "Content-Type": "application/json",
-        }
-    });
+    return await axios.post(orderEndpoint, {order})
 }
